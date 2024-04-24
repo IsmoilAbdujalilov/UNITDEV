72d0cc3a05d32b06861a467974d0e775f1b1fe19
Real-proyektlar <abdujalilovismoilgithub@gmail.com>
2024-04-25 01:22:54 +0300

    feat: Added Teacher component

--- src/assets/images/png/index.ts
+++ src/assets/images/png/index.ts
@@ -5,11 +5,21 @@ import {
   CourseImage4,
   CourseImageUser,
 } from "./course";
+import {
+  TeacherImage1,
+  TeacherImage2,
+  TeacherImage3,
+  TeacherImage4,
+} from "./teachers";
 
 export {
   CourseImage1,
   CourseImage2,
   CourseImage3,
   CourseImage4,
+  TeacherImage1,
+  TeacherImage2,
+  TeacherImage3,
+  TeacherImage4,
   CourseImageUser,
 };
---
+++ src/assets/images/png/teachers/index.ts
@@ -0,0 +1,6 @@
+import TeacherImage1 from "./teachers-image-1.png";
+import TeacherImage2 from "./teachers-image-2.png";
+import TeacherImage3 from "./teachers-image-3.png";
+import TeacherImage4 from "./teachers-image-4.png";
+
+export { TeacherImage1, TeacherImage2, TeacherImage3, TeacherImage4 };
